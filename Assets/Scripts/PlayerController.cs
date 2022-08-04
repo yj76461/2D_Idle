e2d900cba8d97818d1f4925da74b6a7856bceced
yj76461 <yj76461@gmail.com>
2022-08-05 00:12:32 +0900

    animation tangent constant

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -63,10 +63,10 @@ public class PlayerController : MonoBehaviour
             if(canAttack == true){
                 enemyHP = col.GetComponent<EnemyController>().TakeDamage(dmg);
                 canAttack = false;
-                StartCoroutine("AttackDelay");
+                StartCoroutine("AttackDelay"); // 딜레이줘서 한번에 여러대 때리기 방지
             }
 
-            Debug.Log(enemyHP);
+            Debug.Log(enemyHP); // 현 어택 속도일 때, 한번의 공격에서 네번의 충돌 발생 확인.
             if(enemyHP == 0 && canSpawn == true)
             {
                 canSpawn = false;
